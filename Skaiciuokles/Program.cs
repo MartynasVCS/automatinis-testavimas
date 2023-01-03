9369898f2c48ea5d23925a4099b4b8fb88139e7b
MartynasVCS <martynasvcs@gmail.com>
2023-01-03 13:50:46 +0200

    Pridetas skaiciuokliu kodas

---
+++ Skaiciuokles/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Skaiciuokles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Skaiciuokles/Skaiciuokles.cs
@@ -0,0 +1,25 @@
+﻿namespace Skaiciuokles
+{
+    internal class Skaiciuokles
+    {
+        public static int Suma(int sk1, int sk2)
+        {
+            return sk1 + sk2;
+        }
+
+        public static int Atimtis(int sk1, int sk2)
+        {
+            return sk1 - sk2;
+        }
+
+        public static int Daugyba(int sk1, int sk2)
+        {
+            return sk1 * sk2;
+        }
+
+        public static double Dalyba(int sk1, int sk2)
+        {
+            return sk1 / sk2;
+        }
+    }
+}
