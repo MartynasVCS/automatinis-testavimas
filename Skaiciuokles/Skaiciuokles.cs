4222620170fe170cd8c64c40fbba280cc2d6598a
MartynasVCS <martynasvcs@gmail.com>
2023-01-03 21:39:30 +0200

    Papildyta skaičiuoklės programa bei pridėti unit testai

--- Skaiciuokles/Skaiciuokles.cs
+++ Skaiciuokles/Skaiciuokles.cs
@@ -1,10 +1,27 @@
-﻿namespace Skaiciuokles
+﻿using System;
+
+namespace Skaiciuokles
 {
     public class Skaiciuokles
     {
-        public static int Suma(int sk1, int sk2)
+        public static int SudetisInt(int sk1, int sk2)
         {
             return sk1 + sk2;
+        }
+
+        public static double SudetisDouble2(double sk1, double sk2)
+        {
+            return (double)sk1 + sk2;
+        }
+
+        public static double SudetisDouble3(double sk1, double sk2, double sk3)
+        {
+            return (double)sk1 + sk2 + sk3;
+        }
+
+        public static decimal SudetisDecimal3(decimal sk1, decimal sk2, decimal sk3)
+        {
+            return sk1 + sk2 + sk3;
         }
 
         public static int Atimtis(int sk1, int sk2)
@@ -17,9 +34,14 @@
             return sk1 * sk2;
         }
 
-        public static double Dalyba(int sk1, int sk2)
+        public static decimal DalybaDecimal(int sk1, int sk2)
         {
-            return sk1 / sk2;
+            return (decimal)sk1 / sk2;
+        }
+
+        public static double DalybaDouble(int sk1, int sk2) 
+        { 
+            return (double)sk1 / sk2;
         }
     }
 }
--- UnitTestai/SkaiciuoklesTestai.cs
+++ UnitTestai/SkaiciuoklesTestai.cs
@@ -1,20 +1,97 @@
 ﻿using NUnit.Framework;
+using System;
 
 namespace UnitTestai
 {
     public class SkaiciuoklesTestai
     {
         [Test]
-        public void SumosTestas()
+        public void Sudetis_5_5_Int()
         {
-            // Duomenys
             int laukiamaSuma = 10;
 
-            // Veiksmai
-            int tikrojiSuma = Skaiciuokles.Skaiciuokles.Suma(5, 5);
+            int tikrojiSuma = Skaiciuokles.Skaiciuokles.SudetisInt(5, 5);
 
-            // Patikrinimas
             Assert.AreEqual(laukiamaSuma, tikrojiSuma);
+        }
+
+        [Test]
+        public void Atimtis_15_5_Int()
+        {
+            int expectedResult = 10;
+
+            int actualResult = Skaiciuokles.Skaiciuokles.Atimtis(15, 5);
+
+            Assert.IsTrue(expectedResult == actualResult,
+                $"Tikėjausi: {expectedResult}, gavau: {actualResult}");
+        }
+
+        [Test]
+        public void Daugyba_4_2_Int()
+        {
+            int expectedResult = 8;
+
+            int actualResult = Skaiciuokles.Skaiciuokles.Daugyba(4, 2);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void Dalyba_6_4_Double()
+        {
+            double expectedResult = 1.5;
+
+            double actualResult = Skaiciuokles.Skaiciuokles.DalybaDouble(6, 4);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void Dalyba_6_4_Decimal()
+        {
+            Assert.AreEqual(1.5m, Skaiciuokles.Skaiciuokles.DalybaDecimal(6, 4));
+        }
+
+        [Test]
+        public void Dalyba_8_2_Decimal()
+        {
+            Assert.AreEqual(2m, Skaiciuokles.Skaiciuokles.DalybaDecimal(8, 4));
+        }
+
+        [Test]
+        public void Dalyba_1_3_Decimal()
+        {
+            Assert.AreEqual(0.3333333333333333333333333333m, Skaiciuokles.Skaiciuokles.DalybaDecimal(1, 3));
+        }
+
+        [Test]
+        public void Dalyba_1_3_Double()
+        {
+            Assert.AreEqual(0.33333333333333333d, Skaiciuokles.Skaiciuokles.DalybaDouble(1, 3));
+        }
+
+        [Test]
+        public void Dalyba_1_10_Double()
+        {
+            Assert.AreEqual(0.1, Skaiciuokles.Skaiciuokles.DalybaDouble(1, 10));
+        }
+
+        [Test]
+        public void Sudetis_01_01_Double()
+        {
+            Assert.AreEqual(0.2d, Skaiciuokles.Skaiciuokles.SudetisDouble2(0.1, 0.1));
+        }
+
+        [Test]
+        public void Sudetis_01_01_01_Double()
+        {
+            Assert.AreEqual(0.3d, Skaiciuokles.Skaiciuokles.SudetisDouble3(0.1, 0.1, 0.1));
+        }
+
+        [Test]
+        public void Sudetis_01_01_01_Decimal()
+        {
+            Assert.AreEqual(0.3m, Skaiciuokles.Skaiciuokles.SudetisDecimal3(0.1m, 0.1m, 0.1m));
         }
     }
 }
