34168d4c7883ad115d3c18e99b73d21a69779cbf
MartynasVCS <martynasvcs@gmail.com>
2023-01-04 10:44:06 +0200

    Pridėtas pirmas chromedriver testas

---
+++ SeleniumTestai/Class1.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTestai
+{
+    public class Class1
+    {
+        [Test]
+        public void Testas()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://www.google.com";
+        }
+    }
+}
