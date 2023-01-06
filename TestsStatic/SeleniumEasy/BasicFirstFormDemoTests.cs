e5e50ce56af33cd8b5bd6dd4b01503e0ed6bae6d
MartynasVCS <martynasvcs@gmail.com>
2023-01-06 09:25:29 +0200

    Perrašyti BasicFirstFormDemo testai naudojant POM

--- TestsStatic/SeleniumEasy/BasicFirstFormDemoTests.cs
+++ TestsStatic/SeleniumEasy/BasicFirstFormDemoTests.cs
@@ -1,12 +1,73 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FrameworkStatic;
+using FrameworkStatic.Pages.SeleniumEasy;
+using NUnit.Framework;
 
 namespace TestsStatic.SeleniumEasy
 {
     internal class BasicFirstFormDemoTests
     {
+        [SetUp]
+        public void SetUp()
+        {
+            Driver.Initialize();
+            BasicFirstFormDemo.Open();
+        }
+
+        [Test]
+        public void SeleniumEasySingleInputFieldValueOnly()
+        {
+            string expectedResult = "Martynas";
+
+            BasicFirstFormDemo.EnterMessage(expectedResult);
+            BasicFirstFormDemo.ClickShowMessageButton();
+            string actualResult = BasicFirstFormDemo.GetValueOfMessage();
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void SeleniumEasySingleInputFieldFullMessage()
+        {
+            string message = "Martynas";
+            string expectedResult = $"Your Message: {message}";
+
+            BasicFirstFormDemo.EnterMessage(message);
+            BasicFirstFormDemo.ClickShowMessageButton();
+            string actualResult = BasicFirstFormDemo.GetValueOfFullMessage();
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void SeleniumEasyTwoInputFields()
+        {
+            string valueInputA = "3";
+            string valueInputB = "4";
+            string expectedResult = "7";
+
+            BasicFirstFormDemo.EnterValueA(valueInputA);
+            BasicFirstFormDemo.EnterValueB(valueInputB);
+            BasicFirstFormDemo.ClickGetTotalButton();
+            string actualResult = BasicFirstFormDemo.GetValueOfTotal();
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void SeleniumEasyTwoInputFieldsNaN()
+        {
+            string expectedResult = "NaN";
+
+            BasicFirstFormDemo.ClickGetTotalButton();
+            string actualResult = BasicFirstFormDemo.GetValueOfTotal();
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            Driver.CloseDriver();
+        }
     }
 }
