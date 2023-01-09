d1cad31783c8420150eb6a843e64ab296753e2cc
MartynasVCS <martynasvcs@gmail.com>
2023-01-09 16:14:53 +0200

    Pašalintas nereikalingas import

--- FrameworkStatic/Pages/Common.cs
+++ FrameworkStatic/Pages/Common.cs
@@ -1,6 +1,5 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Interactions;
-using OpenQA.Selenium.Support.Extensions;
 using OpenQA.Selenium.Support.UI;
 using SeleniumExtras.WaitHelpers;
 using System;
