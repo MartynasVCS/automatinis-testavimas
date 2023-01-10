6eaa4378ec2973f58007fc0283e4afc482a8ce22
MartynasVCS <martynasvcs@gmail.com>
2023-01-10 17:04:32 +0200

    Ištrinti nereikalingi imports

--- TestsStatic/Facebook/LoginTests.cs
+++ TestsStatic/Facebook/LoginTests.cs
@@ -1,11 +1,6 @@
 ﻿using FrameworkStatic;
 using FrameworkStatic.Pages.Facebook;
 using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TestsStatic.Facebook
 {
